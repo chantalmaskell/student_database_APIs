2450b69fbd54d1097a125e7e6185078672d4df64
Niroshan De Croos Fernandez, Anthony <a.niroshan-de-croos-fernandez@liverpool.ac.uk>
2023-05-04 01:13:45 +0400

    Added code comments

--- auth/auth.js
+++ auth/auth.js
@@ -1,8 +1,11 @@
+//Imports JWT package
 const jwt = require('jsonwebtoken');
 
 // Middleware to check if the user is authenticated
 module.exports = {
 
+    // Reusable function to check if the user is authenticated
+    // All API calls that require authentication will call this function
     isAuthenticated: function (req, res, next) {
         if (typeof req.headers.authorization !== "undefined") {
             let token = req.headers.authorization.split(" ")[1];
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Import express Package
 const express = require('express')
 const app = express()
 
@@ -5,18 +6,23 @@ const app = express()
 app.use(express.urlencoded({extended: true}));
 app.use(express.json())
 
-//Loads environment variables from a .env
+//Loads config into environment variables
 const dotenv = require('dotenv');
 dotenv.config({ path: './config/config.env' });
 
+//Imports JWT package
 const jwt = require('jsonwebtoken');
 
+// Imports the MySQL2 package
+// This import mysql2/promise to use async/await with MySQL
 const mysql = require('mysql2/promise');
 
+// Imports authentication middleware
 const { isAuthenticated } = require('./auth/auth');
 
 let mainConnection = null;
 
+// Database connection
 async function ConnectToDatabase() {
 
   //Connect to Database
@@ -33,9 +39,13 @@ async function ConnectToDatabase() {
   });
 }
 
+// Connect to database
 ConnectToDatabase();
 
-//Generate JWT token for API Calls
+// Generate JWT token for API Calls
+// This is just for testing purposes
+// You can use this token to test the API calls
+// In Production, you will need to generate the token from the login process
 app.get('/jwt', (req, res) => {
   let token = jwt.sign({ "auth": "project" }, process.env.PRIVATE_KEY, { algorithm: process.env.ALGORITHM});
   res.send(token);
@@ -49,10 +59,12 @@ app.get('/', async (req, res) => {
 //Admin course availability
 app.post('/admin/course/availability', isAuthenticated, async (req, res) => {
 
+  // Access post parameters
   const userID = req.body.userID;
   const courseID = req.body.courseID;
   const course_isAvailable = req.body.isAvailable;
 
+  // Input validation
   if (userID == null || course_isAvailable == null || courseID == null) {
     res.status(400).json({ "error": "Invalid request" });
   }
@@ -68,7 +80,8 @@ app.post('/admin/course/availability', isAuthenticated, async (req, res) => {
   else {
 
     try {
-      //Prepare SQL statement. Prevent SQL injection
+      // Prepare SQL statement. Prevent SQL injection
+      // Check user type is admin and authorized to access this API
       let sql = 'SELECT u.UserID, r.Role FROM users u, roles r WHERE u.userID = ? AND u.RoleID = r.RoleID AND r.Role = ?'
       const [userType, _]  = await mainConnection.execute(sql, [userID, "Admin"]);
       if (userType.length == 0) {
@@ -76,19 +89,26 @@ app.post('/admin/course/availability', isAuthenticated, async (req, res) => {
       }
       else {
 
+        // Check if course ID is valid
         let sql = 'SELECT CourseID FROM courses WHERE CourseID = ?'
         const [course, _]  = await mainConnection.execute(sql, [courseID]);
         if (course.length == 0) {
           res.status(400).json({ "error": "Invalid course ID" });
         }
         else {
+
+          // Update course availability
           let sql = 'UPDATE courses SET isAvailable = ? WHERE CourseID = ?'
           await mainConnection.execute(sql, [course_isAvailable, courseID]);
+
+          // Send success response
           res.status(200).json({ "status": "success" });
         }
       }
     }
     catch (err) {
+
+      // Handle Internal server error
       const jsonContent = {error: err.message, name: err.name}
       res.status(500).json(jsonContent);
     }
@@ -98,10 +118,12 @@ app.post('/admin/course/availability', isAuthenticated, async (req, res) => {
 //Admin assign course to teachers
 app.post('/admin/course/assign', isAuthenticated, async (req, res) => {
 
+  // Access post parameters
   const userID = req.body.userID;
   const teacherID = req.body.teacherID;
   const courses = req.body.courses;
 
+  // Input validation
   if (userID == null || teacherID == null || courses == null) {
     res.status(400).json({ "error": "Invalid request" });
   }
@@ -114,6 +136,8 @@ app.post('/admin/course/assign', isAuthenticated, async (req, res) => {
   else {
 
     try {
+      // Prepare SQL statement. Prevent SQL injection
+      // Check user type is admin and authorized to access this API
       let sql = 'SELECT u.UserID, r.Role FROM users u, roles r WHERE u.userID = ? AND u.RoleID = r.RoleID AND r.Role = ?'
       const [userType, _]  = await mainConnection.execute(sql, [userID, "Admin"]);
       if (userType.length == 0) {
@@ -121,12 +145,16 @@ app.post('/admin/course/assign', isAuthenticated, async (req, res) => {
       }
       else {
 
+        // Check if teacher ID is valid
         let sql = 'SELECT u.UserID, r.Role FROM users u, roles r WHERE u.userID = ? AND u.RoleID = r.RoleID AND r.Role = ?'
         const [teacher, _]  = await mainConnection.execute(sql, [teacherID, "Teacher"]);
         if (teacher.length == 0) {
           res.status(400).json({ "error": "Invalid teacher ID" });
         }
         else {
+
+          // Loop through the courses and assign to teacher
+          // This API allow multiple courses to be assigned to a teacher
           for (let i = 0; i < courses.length; i++) {
             let courseID = courses[i];
 
@@ -136,11 +164,14 @@ app.post('/admin/course/assign', isAuthenticated, async (req, res) => {
             }
           }
 
+          // Send success response
           res.status(200).json({ "status": "success" });
         }
       }
     }
     catch (err) {
+
+      // Handle Internal server error
       const jsonContent = {error: err.message, name: err.name}
       res.status(500).json(jsonContent);
     }
@@ -150,11 +181,13 @@ app.post('/admin/course/assign', isAuthenticated, async (req, res) => {
 //Teachers can fail or pass a student
 app.post('/teacher/student/result', isAuthenticated, async (req, res) => {
 
+  // Access post parameters
   const studentID = req.body.studentID;
   const teacherID = req.body.teacherID;
   const courseID = req.body.courseID;
   const result = req.body.result;
 
+  // Input validation
   if (studentID == null || teacherID == null || courseID == null || result == null) {
     res.status(400).json({ "error": "Invalid request" });
   }
@@ -176,6 +209,9 @@ app.post('/teacher/student/result', isAuthenticated, async (req, res) => {
   else {
 
     try {
+
+      // Prepare SQL statement. Prevent SQL injection
+      // Check user type is Teacher and authorized to access this API
       let sql = 'SELECT u.UserID, r.Role FROM users u, roles r WHERE u.userID = ? AND u.RoleID = r.RoleID AND r.Role = ?'
       const [userType, userColumn]  = await mainConnection.execute(sql, [teacherID, 'Teacher']);
       if (userType.length == 0) {
@@ -183,12 +219,15 @@ app.post('/teacher/student/result', isAuthenticated, async (req, res) => {
       }
       else{
 
+        // Check if teacher is assigned to the course
         let sql = 'SELECT title FROM courses WHERE TeacherID = ? AND CourseID = ?'
         const [teacher, _]  = await mainConnection.execute(sql, [teacherID, courseID]);
         if (teacher.length == 0) {
           res.status(401).json({ "error": "Unauthorize access" });
         }
         else {
+
+          // Check if student is enrolled to the course
           let sql = 'SELECT CourseID FROM enrolments WHERE CourseID = ? AND UserID = ?'
           const [teacher, _]  = await mainConnection.execute(sql, [courseID, studentID]);
           if (teacher.length == 0) {
@@ -198,12 +237,16 @@ app.post('/teacher/student/result', isAuthenticated, async (req, res) => {
 
             let sql = 'UPDATE enrolments SET Mark = ? WHERE CourseID = ? AND UserID = ?'
             await mainConnection.execute(sql, [result, courseID, studentID]);
+
+            // Send success response
             res.status(200).json({ "status": "success" });
           }
         }
       }
     }
     catch (err) {
+
+      // Handle Internal server error
       const jsonContent = {error: err.message, name: err.name}
       res.status(500).json(jsonContent);
     }
@@ -221,8 +264,12 @@ app.get('/courses', async (req, res) => {
         WHERE courses.isAvailable = 1 AND roles.Role = 'Teacher';
     `
     const [courses, _] = await mainConnection.execute(sql);
+
+    // Send success response
     res.status(200).json(courses);
   } catch (err) {
+
+    // Handle Internal server error
     const jsonContent = {error: err.message, name: err.name};
     res.status(500).json(jsonContent);
   }
